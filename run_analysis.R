c6f2e62d6a50a91a80285a427e0c1cd541b57aaa
satishmylapore <ms.satishbabu@gmail.com>
2017-08-01 19:09:09 -0500

    saved code

--- run_analysis.R
+++ run_analysis.R
@@ -19,13 +19,6 @@ activity_labels <- fread("./UCI HAR Dataset/activity_labels.txt")
 setnames(activity_labels, "V1", "Activity")
 
 ## 1. Merges the training and the test sets to create one data set.
-x_train[,Data := "Train"] ## Adding Train Identifier for Train X Data
-x_test[,Data := "Test"] ## Adding Train Identifier for Test X Data
-y_train[,Data := "Train"] ## Adding Train Identifier for Train Y Data
-y_test[,Data := "Test"] ## Adding Train Identifier for Test Y Data
-subject_test[,Data := "Test"] ## Adding Train Identifier for Subject test Data
-subject_train[,Data := "Train"] ## Adding Train Identifier for Subject Train Data
-
 
 merged_data_x <- bind_rows(x_train,x_test) # merging data using dplyr lib
 merged_data_y <- bind_rows(y_train,y_test) # merging data using dplyr lib
@@ -40,7 +33,6 @@ matches <- features[V2 %like% "mean|std|Mean|Std"]
 ## updating column Identifier and limiting data to the identified mean and std columns
 matches[,V1:= paste("V",V1, sep = "")]
 matchedcolumns <- matches[,V1]
-matchedcolumns <- c(matchedcolumns, "Data")
 limited_x_data <- merged_data_x[,..matchedcolumns] ## limited columns dataset of x
 
 ## Assuming the records in the Y dataset and Subject is in the same order of rows in X.
@@ -48,7 +40,7 @@ limited_x_data <- merged_data_x[,..matchedcolumns] ## limited columns dataset of
 full_dataset <- limited_x_data[, Activity:= merged_data_y[,V1]] # merge x,y
 full_dataset <- full_dataset[, Subject:= merged_subject[,V1]] # merge x,y,subject
 full_dataset <- as.data.table(left_join(full_dataset,activity_labels, by="Activity", x.all = TRUE, suffix=c("","Y"))) # add activity labels
-full_dataset <- full_dataset[,c("Activity", "V1Y"):= NULL]
+full_dataset <- full_dataset[,c("Activity"):= NULL]
 setnames(full_dataset, "V2Y", "Activity")
 
 matches[,V2:= gsub("\\()",  "", V2)]
